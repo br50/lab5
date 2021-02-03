86a061bc28fdd6243079b5e032133d7763d83fdc
Brian Leung <bleung@ucsd.edu>
2021-02-02 18:07:27 -0800

    Step 5 completed

--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,23 @@ var data = require("../data.json");
 
 exports.addFriend = function(request, response) {    
 	// Your code goes here
+	// I had to look this up
+	console.log(request.query.name);
+	console.log(request.query.description);
+	// JSON for new friend
+	// create new temp js object
+	var newFriendTemp =  new Object();
+	// put name and description of obj using request.query.name
+	newFriendTemp.name = request.query.name;
+	newFriendTemp.description = request.query.description;
+	newFriendTemp.imageURL = 'https://loremflickr.com/500/500/girl?lock=4242';
+
+	var newFriend = JSON.stringify(newFriendTemp);
+	console.log(newFriend);
+
+	data.friends.push(JSON.parse(newFriend));
+
+	console.log(data);
+
+	response.render('index', data);
  }
